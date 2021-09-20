e38272dba24fdb040eafa5161d703b5420fa7822
ANKIT RAJA <65934545+ankitraja786@users.noreply.github.com>
2021-09-20 09:54:36 +0530

    Create Reverse Bits.java

---
+++ Reverse Bits.java
@@ -0,0 +1,13 @@
+ublic class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++){
+            ans = ans << 1;
+            if((n & 1) == 1)
+                ans++;
+            n = n >>> 1;
+        }
+        return ans;
+    }
+}
