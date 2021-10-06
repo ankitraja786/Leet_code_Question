085719bfaa2eb7751e3b5156b9685124128ed855
ANKIT RAJA <65934545+ankitraja786@users.noreply.github.com>
2021-10-06 08:38:41 +0530

    Create duplicate number.java

---
+++ duplicate number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        
+        List<Integer> p = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i] == nums[i+1])
+                p.add(nums[i]);
+            }
+            return p;
+        }
+    
+}
