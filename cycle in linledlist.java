998951c333493fa185379c93e901c107c634968e
ANKIT RAJA <65934545+ankitraja786@users.noreply.github.com>
2021-10-12 00:20:12 +0530

    Create cycle in linledlist.java

---
+++ cycle in linledlist.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next== null)
+            return false;
+        ListNode l1= head;
+        ListNode l2= head.next;
+        
+        while(l2!=null && l2.next!=null){
+            if(l1==l2)
+                return true;
+            l1 = l1.next;
+            l2 = l2.next.next;
+        }
+        return false;
+    }
+    
+}
